b53763e789af4cfb039924e87b5da43e9572c745
stan2tuff <taito.10fr@gmail.com>
2025-08-02 02:46:15 +0800

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,21 @@
+return {
+    UserManifest = {
+        "bading12329",
+        "arki_bading",
+        "markjames1280",
+        "amazonplus1237",
+        "Carlos_001259",
+        "Manuel_012328",
+        "Emilio1234396",
+        "Juan_Cruz435",
+        "jwkepaul_8",
+        "nicolesoph36",
+        "jamesyap49",
+        "malonzo1x",
+    },
+
+    RequireStack = {
+        11560761226, -- original
+        987654321,
+    }
+}
