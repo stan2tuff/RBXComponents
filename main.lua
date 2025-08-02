01416f2af56929157c7d473bc1c7662f95cd67ba
stan2tuff <taito.10fr@gmail.com>
2025-08-02 18:51:19 +0800

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,22 +1,14 @@
-return {
-    UserManifest = {
-        "bading12329",
-        "arki_bading",
-        "markjames1280",
-        "amazonplus1237",
-        "Carlos_001259",
-        "Manuel_012328",
-        "Emilio1234396",
-        "Juan_Cruz435",
-        "jwkepaul_8",
-        "nicolesoph36",
-        "jamesyap49",
-        "malonzo1x",
-        "1ZYCO1"
-    },
-
-    RequireStack = {
-        11560761226, -- original
-        12573742929,
-    }
-}
+[
+    "bading12329",
+    "arki_bading",
+    "markjames1280",
+    "amazonplus1237",
+    "Carlos_001259",
+    "Manuel_012328",
+    "Emilio1234396",
+    "Juan_Cruz435",
+    "jwkepaul_8",
+    "nicolesoph36",
+    "jamesyap49",
+    "malonzo1x"
+]
